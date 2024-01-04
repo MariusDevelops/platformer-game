6305253d2a0c97c69068fb767213e99582b113c6
MariusDevelops <brazasmarius@gmail.com>
2024-01-04 17:38:48 +0200

    feat: add animations

--- client/src/main/java/org/example/inputs/KeyboardInputs.java
+++ client/src/main/java/org/example/inputs/KeyboardInputs.java
@@ -4,9 +4,11 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import org.example.main.GamePanel;
+import static org.example.utilz.Constants.Directions.*;
 
 public class KeyboardInputs implements KeyListener {
-    private final GamePanel gamePanel;
+
+    private GamePanel gamePanel;
 
     public KeyboardInputs(GamePanel gamePanel) {
         this.gamePanel = gamePanel;
@@ -14,21 +16,36 @@ public class KeyboardInputs implements KeyListener {
 
     @Override
     public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_W:
+            case KeyEvent.VK_A:
+            case KeyEvent.VK_S:
+            case KeyEvent.VK_D:
+                gamePanel.setMoving(false);
+                break;
+        }
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
         switch (e.getKeyCode()) {
-            case KeyEvent.VK_W -> gamePanel.changeYDelta(-5);
-            case KeyEvent.VK_A -> gamePanel.changeXDelta(-5);
-            case KeyEvent.VK_S -> gamePanel.changeYDelta(5);
-            case KeyEvent.VK_D -> gamePanel.changeXDelta(5);
-            default -> System.out.println("wrong key pressed");
+            case KeyEvent.VK_W:
+                gamePanel.setDirection(UP);
+                break;
+            case KeyEvent.VK_A:
+                gamePanel.setDirection(LEFT);
+                break;
+            case KeyEvent.VK_S:
+                gamePanel.setDirection(DOWN);
+                break;
+            case KeyEvent.VK_D:
+                gamePanel.setDirection(RIGHT);
+                break;
         }
     }
-
 }
--- client/src/main/java/org/example/inputs/MouseInputs.java
+++ client/src/main/java/org/example/inputs/MouseInputs.java
@@ -7,7 +7,7 @@ import java.awt.event.MouseMotionListener;
 import org.example.main.GamePanel;
 
 public class MouseInputs implements MouseListener, MouseMotionListener {
-    private final GamePanel gamePanel;
+    private GamePanel gamePanel;
 
     public MouseInputs(GamePanel gamePanel) {
         this.gamePanel = gamePanel;
@@ -15,31 +15,42 @@ public class MouseInputs implements MouseListener, MouseMotionListener {
 
     @Override
     public void mouseDragged(MouseEvent e) {
+        // TODO Auto-generated method stub
+
     }
 
     @Override
     public void mouseMoved(MouseEvent e) {
-        gamePanel.setRectPos(e.getX(), e.getY());
+
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {
+
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+
     }
 
     @Override
     public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+
     }
 
     @Override
     public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+
     }
 
 }
