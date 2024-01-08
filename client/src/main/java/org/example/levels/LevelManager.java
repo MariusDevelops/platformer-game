b7cb44046168651ea0343969b8c2e336877455e9
MariusDevelops <brazasmarius@gmail.com>
2024-01-08 22:54:13 +0200

    fix: change levelManager file name

--- client/src/main/java/org/example/levels/LevelManager.java
+++ client/src/main/java/org/example/levels/LevelManager.java
@@ -1,4 +1,4 @@
-package org.example.levels
+package org.example.levels;
 
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
@@ -22,18 +22,18 @@ public class LevelManager {
         BufferedImage img = LoadSave.GetSpriteAtlas(LoadSave.LEVEL_ATLAS);
         levelSprite = new BufferedImage[48];
         for (int j = 0; j < 4; j++)
-        for (int i = 0; i < 12; i++) {
-            int index = j * 12 + i;
-            levelSprite[index] = img.getSubimage(i * 32, j * 32, 32, 32);
-        }
+            for (int i = 0; i < 12; i++) {
+                int index = j * 12 + i;
+                levelSprite[index] = img.getSubimage(i * 32, j * 32, 32, 32);
+            }
     }
 
     public void draw(Graphics g) {
         for (int j = 0; j < Game.TILES_IN_HEIGHT; j++)
-        for (int i = 0; i < Game.TILES_IN_WIDTH; i++) {
-            int index = levelOne.getSpriteIndex(i, j);
-            g.drawImage(levelSprite[index], Game.TILES_SIZE * i, Game.TILES_SIZE * j, Game.TILES_SIZE, Game.TILES_SIZE, null);
-        }
+            for (int i = 0; i < Game.TILES_IN_WIDTH; i++) {
+                int index = levelOne.getSpriteIndex(i, j);
+                g.drawImage(levelSprite[index], Game.TILES_SIZE * i, Game.TILES_SIZE * j, Game.TILES_SIZE, Game.TILES_SIZE, null);
+            }
     }
 
     public void update() {
